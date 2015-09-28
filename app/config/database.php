8c8806ecf26fcb9018af243124ae71fd7bc7d41c
Graeme Tait <burriko@gmail.com>
2015-09-28 10:43:48 +0100

    Missed host

--- app/config/database.php
+++ app/config/database.php
@@ -4,6 +4,7 @@ return [
 
     'connection' => [
         'driver'   => 'pdo_mysql',
+        'host'     => getenv('DB_HOST'),
         'user'     => getenv('DB_USERNAME'),
         'password' => getenv('DB_PASSWORD'),
         'dbname'   => getenv('DB_DATABASE'),
